641152658869a47618db059ce6c6955e7a487bd5
vuong <vuong03062003@gmail.com>
2023-10-15 22:56:29 +0700

    First commit

---
+++ lab3_ex/Core/Inc/global.h
@@ -0,0 +1,24 @@
+/*
+ * global.h
+ *
+ *  Created on: Oct 11, 2023
+ *      Author: Acer
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+extern int led_status;
+extern int traffic_status;
+extern int led7seg_status;
+extern int led7buffer[4];
+extern int OUT;
+extern int blink_state;
+extern int red_timer;
+extern int yellow_timer;
+extern int green_timer;
+extern int red_timer_mode;
+extern int yellow_timer_mode;
+extern int green_timer_mode;
+
+#endif /* INC_GLOBAL_H_ */
